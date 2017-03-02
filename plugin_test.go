326b31dec7dd9fa369077022282af91ec4b20c87
Alain Gilbert <alain.gilbert.15@gmail.com>
2017-03-02 11:00:08 -0800

    Add some tests

--- plugin_test.go
+++ plugin_test.go
@@ -1,1 +1,49 @@
 package plugin_test
+
+import (
+	"testing"
+
+	"github.com/alaingilbert/go-plugin"
+)
+
+func TestCall(t *testing.T) {
+	plugin.Init()
+	defer plugin.Close()
+	p, _ := plugin.Load("plugin_test.lua")
+	ret, err := p.Call("Square", 3)
+	if err != nil {
+		t.Error("Unable to call Square", err)
+	}
+	if ret.String() != "9" {
+		t.Error("Should be 9", ret.String())
+	}
+}
+
+func TestCallUnmarshal(t *testing.T) {
+	plugin.Init()
+	defer plugin.Close()
+	p, _ := plugin.Load("plugin_test.lua")
+
+	// Test with number
+	var squared int
+	if err := p.CallUnmarshal(&squared, "Square", 3); err != nil {
+		t.Error("There should be no error", err)
+	}
+	if squared != 9 {
+		t.Error("squared should be 9", squared)
+	}
+
+	// Test with string
+	var stringTest string
+	p.CallUnmarshal(&stringTest, "StringTest")
+	if stringTest != "a string" {
+		t.Error("stringTest should be 'a string'", stringTest)
+	}
+
+	// Test with bool
+	var boolTest bool
+	p.CallUnmarshal(&boolTest, "BoolTest")
+	if boolTest != true {
+		t.Error("boolTest should be true", boolTest)
+	}
+}
---
+++ plugin_test.lua
@@ -0,0 +1,11 @@
+function Square(x)
+    return x * x
+end
+
+function StringTest()
+    return "a string"
+end
+
+function BoolTest()
+    return true
+end
