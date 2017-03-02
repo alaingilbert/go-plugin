38d244f783ab07cb1d8533be3ab66fc76c648dd3
Alain Gilbert <alain.gilbert.15@gmail.com>
2017-03-02 01:23:20 -0800

    deps

---
+++ vendor/layeh.com/gopher-luar/map.go
@@ -0,0 +1,114 @@
+package luar
+
+import (
+	"reflect"
+
+	"github.com/yuin/gopher-lua"
+)
+
+func mapIndex(L *lua.LState) int {
+	ref, mt, isPtr := check(L, 1, reflect.Map)
+	key := L.CheckAny(2)
+
+	if isPtr {
+		if lstring, ok := key.(lua.LString); ok {
+			if fn := mt.ptrMethod(string(lstring)); fn != nil {
+				L.Push(fn)
+				return 1
+			}
+		}
+		return 0
+	}
+
+	convertedKey := lValueToReflect(L, key, ref.Type().Key(), nil)
+	if convertedKey.IsValid() {
+		item := ref.MapIndex(convertedKey)
+		if item.IsValid() {
+			L.Push(New(L, item.Interface()))
+			return 1
+		}
+	}
+
+	if !isPtr {
+		if lstring, ok := key.(lua.LString); ok {
+			if fn := mt.method(string(lstring)); fn != nil {
+				L.Push(fn)
+				return 1
+			}
+		}
+	}
+
+	if lstring, ok := key.(lua.LString); ok {
+		if fn := mt.ptrMethod(string(lstring)); fn != nil {
+			L.Push(fn)
+			return 1
+		}
+	}
+
+	return 0
+}
+
+func mapNewIndex(L *lua.LState) int {
+	ref, _, isPtr := check(L, 1, reflect.Map)
+	if isPtr {
+		L.RaiseError("invalid operation on map pointer")
+	}
+	key := L.CheckAny(2)
+	value := L.CheckAny(3)
+
+	convertedKey := lValueToReflect(L, key, ref.Type().Key(), nil)
+	if !convertedKey.IsValid() {
+		L.ArgError(2, "invalid map key")
+	}
+	var convertedValue reflect.Value
+	if value != lua.LNil {
+		convertedValue = lValueToReflect(L, value, ref.Type().Elem(), nil)
+		if !convertedValue.IsValid() {
+			L.ArgError(3, "invalid map value")
+		}
+	}
+	ref.SetMapIndex(convertedKey, convertedValue)
+	return 0
+}
+
+func mapLen(L *lua.LState) int {
+	ref, _, isPtr := check(L, 1, reflect.Map)
+	if isPtr {
+		L.RaiseError("invalid operation on map pointer")
+	}
+	L.Push(lua.LNumber(ref.Len()))
+	return 1
+}
+
+func mapCall(L *lua.LState) int {
+	ref, _, isPtr := check(L, 1, reflect.Map)
+	if isPtr {
+		L.RaiseError("invalid operation on map pointer")
+	}
+	keys := ref.MapKeys()
+	i := 0
+	fn := func(L *lua.LState) int {
+		if i >= len(keys) {
+			return 0
+		}
+		L.Push(New(L, keys[i].Interface()))
+		L.Push(New(L, ref.MapIndex(keys[i]).Interface()))
+		i++
+		return 2
+	}
+	L.Push(L.NewFunction(fn))
+	return 1
+}
+
+func mapEq(L *lua.LState) int {
+	ref1, _, isPtr1 := check(L, 1, reflect.Map)
+	ref2, _, isPtr2 := check(L, 2, reflect.Map)
+
+	if isPtr1 && isPtr2 {
+		L.Push(lua.LBool(ref1.Pointer() == ref2.Pointer()))
+		return 1
+	}
+
+	L.RaiseError("invalid operation == on map")
+	return 0 // never reaches
+}
---
+++ vendor/layeh.com/gopher-luar/struct.go
@@ -0,0 +1,64 @@
+package luar
+
+import (
+	"reflect"
+
+	"github.com/yuin/gopher-lua"
+)
+
+func structIndex(L *lua.LState) int {
+	ref, mt, isPtr := check(L, 1, reflect.Struct)
+	key := L.CheckString(2)
+
+	if !isPtr {
+		if fn := mt.method(key); fn != nil {
+			L.Push(fn)
+			return 1
+		}
+	}
+
+	if fn := mt.ptrMethod(key); fn != nil {
+		L.Push(fn)
+		return 1
+	}
+
+	ref = reflect.Indirect(ref)
+	index := mt.fieldIndex(key)
+	if index == nil {
+		return 0
+	}
+	field := ref.FieldByIndex(index)
+	if !field.CanInterface() {
+		L.RaiseError("cannot interface field " + key)
+	}
+
+	if (field.Kind() == reflect.Struct || field.Kind() == reflect.Array) && field.CanAddr() {
+		field = field.Addr()
+	}
+	L.Push(New(L, field.Interface()))
+	return 1
+}
+
+func structNewIndex(L *lua.LState) int {
+	ref, mt, isPtr := check(L, 1, reflect.Struct)
+	if isPtr {
+		ref = ref.Elem()
+	}
+	key := L.CheckString(2)
+	value := L.CheckAny(3)
+
+	index := mt.fieldIndex(key)
+	if index == nil {
+		L.RaiseError("unknown field " + key)
+	}
+	field := ref.FieldByIndex(index)
+	if !field.CanSet() {
+		L.RaiseError("cannot set field " + key)
+	}
+	val := lValueToReflect(L, value, field.Type(), nil)
+	if !val.IsValid() {
+		L.ArgError(2, "invalid value")
+	}
+	field.Set(val)
+	return 0
+}
